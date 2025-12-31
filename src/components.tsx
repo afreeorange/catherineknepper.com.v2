58e2e4de16da356cabff75aaaeb56c3be1e0824c
Nikhil Anand <mail@nikhil.io>
2025-12-31 13:13:21 -0600

    Shell

--- src/components.tsx
+++ src/components.tsx
@@ -1,29 +1,24 @@
 import type { ComponentChildren } from "preact";
 
+const TITLE = "Catherine Knepper Writing Services, LLC";
+const DESCRIPTION =
+  "I help smart, talented people bring their ideas to the page and to the marketplace.";
+
 export const Shell = ({ children }: { children?: ComponentChildren }) => (
   <html lang="en">
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
       <meta name="view-transition" content="same-origin" />
+      <meta property="og:title" content={TITLE} />
+      <meta property="og:description" content={DESCRIPTION} />
+      <meta property="og:image" content="/catherine-small.jpg" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="http://catherineknepper.com/" />
-      <meta
-        property="og:title"
-        content="Catherine Knepper Writing Services, LLC"
-      />
-      <meta
-        property="og:description"
-        content="I help smart, talented people bring their ideas to the page and to the marketplace."
-      />
-      <meta
-        property="og:image"
-        content="http://catherineknepper.com/catherine-small.jpg"
-      />
 
-      <title>Catherine Knepper Writing Services, LLC.</title>
+      <title>{TITLE}</title>
 
-      <link rel="apple-touch-icon" type="image/png" href="/favicon.png" />
+      <link rel="apple-touch-icon" type="image/svg+xml" href="/favicon.svg" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <link rel="stylesheet" href="/index.css" />
 
@@ -32,10 +27,8 @@ export const Shell = ({ children }: { children?: ComponentChildren }) => (
         defer
         data-domain="catherineknepper.com"
         src="https://plausible.io/js/plausible.js"
-      ></script>
+      />
     </head>
-    <body>
-      {children}
-    </body>
+    <body>{children}</body>
   </html>
 );
