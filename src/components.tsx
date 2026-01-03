de3c386836a90952dae1cdd74356879f473c868f
Nikhil Anand <mail@nikhil.io>
2026-01-03 12:56:05 -0600

    Meta Description

--- src/components.tsx
+++ src/components.tsx
@@ -10,6 +10,7 @@ export const Shell = ({ children }: { children?: ComponentChildren }) => (
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
       <meta name="view-transition" content="same-origin" />
+      <meta name="description" content={DESCRIPTION}></meta>
 
       <meta property="og:title" content={TITLE} />
       <meta property="og:description" content={DESCRIPTION} />
